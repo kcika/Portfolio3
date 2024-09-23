9d5b64a672614f8567ac07d964feec89e487690b
kcika <kcbhumika6@gmail.com>
2024-09-23 10:04:04 +0530

    files added

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".toggle").addEventListener("click", () => {
+    document.querySelector(".navlinks").classList.toggle("active")
+})
+document.querySelector(".cross").addEventListener("click", () => {
+    document.querySelector(".navlinks").classList.toggle("active")
+})
